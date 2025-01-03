719dd82bdc873436281792b21c2c71ef5c89214b
Sandeepkollii <71366267+Sandeepkollii@users.noreply.github.com>
2025-01-03 21:28:56 +0530

    Delete coursebatchservice-main directory

--- coursebatchservice-main/TMSClientApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TMSClientApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
